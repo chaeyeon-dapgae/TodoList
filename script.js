51cab2bee5373a8774c61323ba73196997cb4828
chaeyeon <cysy0424@gmail.com>
2024-07-03 18:27:26 +0900

    4th commit

--- script.js
+++ script.js
@@ -9,8 +9,17 @@
 let taskInput = document.getElementById('task-input');
 let addButton = document.getElementById('add-button');
 let taskList = [];
+let tabs = document.querySelectorAll(".task-tabs div");
+let mode = 'all';
+let filterList = [];
 
 addButton.addEventListener("click", addTask);
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function(event){
+    filter(event)
+  })
+}
 
 function addTask () {
   let task = {
@@ -23,25 +32,39 @@ function addTask () {
 }
 
 function render () {
+  // 1. 내가 선택한 탭에 따라서
+  let list = [];
+  if (mode === 'all') {
+    //all taskList
+    list = taskList;
+  } else if (mode === 'ongoing' || mode === 'done') {
+    // ongoing, done filterList
+    list = filterList;
+  }
+  // 2. 리스트를 달리 보여준다
+  // all => taskList
+  // ongoing,done => filterList
+
+
   let resultHtml = '';
 
-  for(let i = 0; i < taskList.length; i ++) {
-    if (taskList[i].isComplete == true) {
+  for(let i = 0; i < list.length; i ++) {
+    if (list[i].isComplete == true) {
       resultHtml += 
       `<div class="task">
-        <div class="task-done">${taskList[i].taskContent}</div>
+        <div class="task-done">${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')"><i class="xi-refresh"></i></button>
-          <button onclick="deleteTask('${taskList[i].id}')"><i class="xi-trash"></i></button>
+          <button onclick="toggleComplete('${list[i].id}')"><i class="xi-refresh"></i></button>
+          <button onclick="deleteTask('${list[i].id}')"><i class="xi-trash"></i></button>
         </div>
       </div>`
     } else {
       resultHtml +=
         `<div class="task">
-          <div>${taskList[i].taskContent}</div>
+          <div>${list[i].taskContent}</div>
           <div>
-            <button onclick="toggleComplete('${taskList[i].id}')"><i class="xi-check"></i></button>
-            <button onclick="deleteTask('${taskList[i].id}')"><i class="xi-trash"></i></button>
+            <button onclick="toggleComplete('${list[i].id}')"><i class="xi-check"></i></button>
+            <button onclick="deleteTask('${list[i].id}')"><i class="xi-trash"></i></button>
           </div>
         </div>`
     }
@@ -68,6 +91,33 @@ function deleteTask(id) {
     }
   }
   render()
+}
+
+function filter(event) {
+  mode = event.target.id
+  filterList = []
+
+  if (mode === "all") {
+    render ()
+  } else if (mode === "ongoing") {
+    //진행중인 리스트를 보여준다.
+    //task.isComplete = false
+    for (let i = 0; i < taskList.length; i++) {
+      if(taskList[i].isComplete === false) {
+        filterList.push(taskList[i])
+      }
+    }
+    render ()
+  } else if (mode === "done") {
+    //끝나는 케이스
+    //task.isComplete = true
+    for(let i = 0; i < taskList.length; i++) {
+      if(taskList[i].isComplete === true) {
+        filterList.push(taskList[i])
+      }
+    }
+    render()
+  } 
 }
 
 function randomIDGenerate () {
